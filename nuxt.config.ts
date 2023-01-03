89ff62d1cae91e96998a58a2f1e24798122576ad
Konstantin Voronoy <konstantinvoronoy@gmail.com>
2023-01-03 23:33:44 +0400

    feat: remove unnecessary tailwind config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,5 @@
 export default defineNuxtConfig({
 	ssr: true,
 	modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
-	css: ['~/assets/css/main.css'],
-	build: {
-		postcss: {
-			postcssOptions: {
-				plugins: {
-					tailwindcss: {},
-					autoprefixer: {},
-				},
-			},
-		},
-	}
+	css: ['~/assets/css/main.css']
 })
