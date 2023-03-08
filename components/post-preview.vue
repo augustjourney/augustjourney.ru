4012b0d7d22b55124ee60cac1ff0f6d4134d3c8e
kostyacodes <konstantinvoronoy@gmail.com>
2023-03-08 21:55:46 +0400

    feat: remove icon on mobile in post preview

--- components/post-preview.vue
+++ components/post-preview.vue
@@ -1,6 +1,6 @@
 <template>
 	<a class="mb-6 block relative" :href="post._path">
-		<div class="absolute left-0 top-0 mt-1 -ml-8">
+		<div class="absolute md:block hidden left-0 top-0 mt-1 -ml-8">
 			<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" :class="hovered ? 'text-indigo-600' : 'text-indigo-200'" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
 			</svg>
